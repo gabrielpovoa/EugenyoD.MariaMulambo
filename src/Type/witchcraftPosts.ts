04526420761280d9fd1e74a500c24d8e812e41ff
João Gabriel <povoajoao6@gmail.com>
2025-07-20 15:12:26 -0300

    fix: changing post image type

--- src/Type/witchcraftPosts.ts
+++ src/Type/witchcraftPosts.ts
@@ -1,6 +1,8 @@
+import {StaticImageData} from "next/image";
+
 export interface witchcraftPosts {
     id: string;
-    imageUrl: string;
+    imageUrl: StaticImageData;
     caption: string;
     content: number;
 }
